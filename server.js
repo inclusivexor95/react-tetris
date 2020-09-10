0008ed0a7e7208d75d0279e8d9ac41cd46d3cd05
inclusivexor95 <mstew033@gmail.com>
2020-09-10 17:17:44 -0400

    change public_url from public to build

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/users', usersRouter);
 app.use(require('./config/auth'));
