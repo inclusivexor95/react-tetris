f6da1c293459f5c1e8146c13f7a080bae5b1b6ae
Matt Stewart <mattystewww033@gmail.com>
2023-02-13 17:09:54 -0500

    Updates

---
+++ precache-manifest.8ca938f8e0593f10f712000bbebad542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16f979c894784149d86f573c726c38f",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "351fc066fa4fc4940b01",
+    "url": "/react-tetris/static/css/main.232536d6.chunk.css"
+  },
+  {
+    "revision": "43af67388a26090908c1",
+    "url": "/react-tetris/static/js/2.21c1d88d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tetris/static/js/2.21c1d88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "351fc066fa4fc4940b01",
+    "url": "/react-tetris/static/js/main.35ce5814.chunk.js"
+  },
+  {
+    "revision": "bf8426f2cbb357ed796e",
+    "url": "/react-tetris/static/js/runtime-main.d5ca2537.js"
+  }
+]);
